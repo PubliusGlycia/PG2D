7b9be1b090e858b8c7cf931a485e3c384cd38d99
Wojciech Oset <209415@edu.p.lodz.pl>
2019-04-09 00:05:46 +0200

    Testy do gry
    Testy

---
+++ Testownica/Testownica/AnimatedSprite.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testownica
+{
+    abstract class AnimatedSprite
+    {
+        protected Texture2D spriteTexture;
+        protected Vector2 spritePosition;
+
+        private Rectangle[] spriteRectangle;
+        private int frameIndex;
+
+        private double timeElapsed;  //określenie ilości czasu który upłynął od wywołania
+        private double timeToUpdate; //określenie ilości czasu po której powinna wykonać funkcja update
+
+        protected Vector2 spriteDirection = Vector2.Zero;
+
+        private string currentAnimation; //holding current animation to compare with dictionarny
+
+        public int FPS               //FramesPerSecond - szybkość animacji
+        {
+            set { timeToUpdate = (1f / value); }
+        }
+
+        public AnimatedSprite(Vector2 position) //constructor
+        {
+            spritePosition = position; 
+        }
+
+        private Dictionary<string, Rectangle[]> spriteAnimations = new Dictionary<string, Rectangle[]>();   //słownik do lepszego dzielenia przekazywanego sprite'a
+
+        public void Animation(int frames, int yPos, int xStart, string name, int width, int height, Vector2 offset)
+        {
+            /*dla prostszych animacji z kilkoma kratkami w poziomie:
+            * obliczanie szerokości kratki
+            * int width = spriteTexture.Width / frames;
+            *
+            * tworzenie tablicy grafik do animacji
+            * spriteRectangle = new Rectangle[frames];
+            *
+            * wypełnianie tablicy grafikami
+            *for(int i = 0; i < frames; i++)
+            *{
+            *    spriteRectangle[i] = new Rectangle(i * width, 0,width,spriteTexture.Height); //tworzenie siatki obiektów grafiki do animacji - do sprawdzenia
+            *}
+            */
+
+            // lepsze cięcie!!!
+            Rectangle[] Rectangles = new Rectangle[frames];
+            for (int i = 0; i < frames; i++)
+                {
+                    Rectangles[i] = new Rectangle((i + xStart) * width, yPos, width, height);
+                }
+            spriteAnimations.Add(name, Rectangles); // adding to dictionary
+        }
+
+        //virtual+override - możliwość nadpisywania poprzez metody inne klasy
+        public virtual void Update(GameTime gameTime) //wykonywanie animacji
+        {
+            timeElapsed += gameTime.ElapsedGameTime.TotalSeconds;
+            if(timeElapsed > timeToUpdate)
+            {
+                timeElapsed -= timeToUpdate; //stały framerate
+
+                if(frameIndex < spriteAnimations[currentAnimation].Length - 1) //wykonanie animacji
+                { 
+                    frameIndex++;
+                }
+                else
+                {
+                    frameIndex = 0;
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch) //'rysowanie' obiektu
+        {
+            spriteBatch.Draw(spriteTexture, spritePosition, spriteAnimations[currentAnimation][frameIndex], Color.White);
+            // parametr spriteAnimations[currentAnimation][frameIndex] ustala obecnie odgrywaną animację oraz rozpoczynający ją indeks
+        }
+
+        public void PlayAnimation(string name)
+        {
+            if(currentAnimation != name)
+            {
+                currentAnimation = name;
+                frameIndex = 0;         // reset w przypadku gdy zmieniamy animację
+            }
+        }
+
+    }
+}
---
+++ Testownica/Testownica/PlayerObject.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testownica
+{
+    class PlayerObject : AnimatedSprite
+    {
+
+        float playerSpeed = 5;    //hero's movement speed
+        
+
+        public PlayerObject(Vector2 position) : base(position) // przekazanie pozycji gracza do pozycji klasy AnimatedSprite
+        {
+            FPS = 6;
+
+            //Wywoływanie animacji
+            Animation(4, 0, 0, "Down", 32, 48, new Vector2(0, 0));
+            Animation(4, 48, 0, "Left", 32, 48, new Vector2(0, 0));
+            Animation(4, 96, 0, "Right", 32, 48, new Vector2(0, 0));
+            Animation(4, 144, 0, "Up", 32, 48, new Vector2(0, 0));
+
+            PlayAnimation("Up");
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            spriteTexture = content.Load<Texture2D>("hiro");
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            spriteDirection = Vector2.Zero;                                 // reset input
+            Input(Keyboard.GetState());                                     // gets the state of my keyborad
+
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds; // make movement framerate independant
+
+            spriteDirection *= playerSpeed;                                 // add hero's speed to movement 
+            spritePosition += (spriteDirection * deltaTime);                // adding deltaTime to stabilize movement
+
+            base.Update(gameTime);                                          // update overriden from AnimatedSprite
+        }
+
+        private void Input(KeyboardState keyState)  //funkcja do poruszania postaci
+        {
+            if (keyState.IsKeyDown(Keys.W))
+            {
+                //Move up 
+                spriteDirection += new Vector2(0, -32);
+                PlayAnimation("Up");
+            }
+            if (keyState.IsKeyDown(Keys.S))
+            {
+                //Move down
+                spriteDirection += new Vector2(0, 32);
+                PlayAnimation("Down");
+            }
+            if (keyState.IsKeyDown(Keys.A))
+            {
+                //Move left
+                spriteDirection += new Vector2(-32, 0);
+                PlayAnimation("Left");
+            }
+            if (keyState.IsKeyDown(Keys.D))
+            {
+                //Move right 
+                spriteDirection += new Vector2(32, 0);
+                PlayAnimation("Right");
+            }
+        }
+
+    }
+}
